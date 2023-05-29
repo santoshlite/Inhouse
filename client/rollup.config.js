db6a9e1c7bc252f85dbee130dcd8c604ff178a71
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2023-05-29 19:33:49 -0400

    Update rollup.config.js

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,50 +1,41 @@
 import svelte from 'rollup-plugin-svelte';
-import resolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
+import sveltePreprocess from 'svelte-preprocess';
+import css from 'rollup-plugin-css-only'
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/main.js',
-	output: {
-		sourcemap: true,
-		format: 'iife',
-		name: 'app',
-		file: 'public/bundle.js'
-	},
-	plugins: [
-		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/bundle.css');
-			}
-		}),
+  input: 'src/main.js',
+  output: {
+    sourcemap: true,
+    format: 'iife',
+    name: 'app',
+    file: 'public/bundle.js',
+  },
+  plugins: [
+    svelte({
+      compilerOptions: {
+        dev: !production,
+      },
+      preprocess: sveltePreprocess(), // Add this line to enable preprocessors
+    }),
+	css({ output: 'public/bundle.css' }),
+    resolve({
+      browser: true,
+      dedupe: ['svelte'],
+    }),
+    commonjs(),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve({
-			browser: true,
-			dedupe: importee => importee === 'svelte' || importee.startsWith('svelte/')
-		}),
-		commonjs(),
-
-		// Watch the `public` directory and refresh the
-		// browser on changes when not in production
-		!production && livereload('public'),
-
-		// If we're building for production (npm run build
-		// instead of npm run dev), minify
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+    // Add the following plugins to handle non-JavaScript files
+    sveltePreprocess(), // Preprocess the Svelte components
+    livereload('public'), // Watch the `public` directory and refresh the browser
+    production && terser(), // Minify the output in production mode
+  ],
+  watch: {
+    clearScreen: false,
+  },
 };
