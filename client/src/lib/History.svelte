ac1feb989beb992b3a04b891196f35b1b21c9fcc
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2023-05-31 21:20:34 -0400

    Create History.svelte

---
+++ client/src/lib/History.svelte
@@ -0,0 +1,16 @@
+
+<script>
+    let history = "History";
+    let dummyList = ["How many invoices do I have?", "Who are my biggest competitors?", "What does my insurance protect me against?", "When is my next payment due?"];
+  </script>
+
+
+<div class="wrapper">
+    <h3>{history}</h3>
+  
+    <div class="list-container">
+      {#each dummyList as item}
+        <div class="history-item">{item}</div>
+      {/each}
+    </div>
+  </div>
