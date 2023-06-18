0c83b6a5ab2d5fd1a152f9d16c07ca9553c7f2ab
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2023-06-17 22:42:56 -0400

    Update +page.svelte

--- frontend/src/routes/[...notfound]/+page.svelte
+++ frontend/src/routes/[...notfound]/+page.svelte
@@ -1,1 +1,15 @@
-<h1>404 Not found</h1>
+<script>
+    import '/src/routes/styles.css';
+</script>
+
+<svelte:head>
+    <title>Inhouse | 404</title>
+    <meta name="description" content="404 error, page not found" />
+</svelte:head>
+
+<div class="wrapper-error">
+    <h3 class="welcome">inhouse 🏠</h3>
+    <h1 class="error">404</h1>
+    <p class="subtext">Oops, there is nothing in here. Try login again!</p>
+    <button><a href="/auth">Access</a></button>
+</div>
