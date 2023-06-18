b2043a9f2e40743dcc933c85b4304f5562b5c2ee
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2023-06-18 17:21:48 -0400

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </svelte:head>
 <div class="flex box" id="welcome">
   <div>
-    <h1>ChatGPT writes. <br> Inhouse reads.</h1>
+    <h1>ChatGPT writes<br> Inhouse reads</h1>
     <a href="/auth">
       <button class="button">TRY IT</button>
     </a>
