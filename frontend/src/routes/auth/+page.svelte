ee0d0d0cf340533ac9c6b2594b6ef3b191b471bb
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2023-06-20 23:01:36 -0400

    Update +page.svelte

--- frontend/src/routes/auth/+page.svelte
+++ frontend/src/routes/auth/+page.svelte
@@ -2,11 +2,23 @@
     // @ts-nocheck
 
     import { onMount } from 'svelte';
-    let url="empty";
-    function getURL(){
-        url = window.location.href;
+    let url = "empty";
+    async function showErrorMessage() {
+        url = window.location.href
+        if (window.location.href === 'https://inhouse.up.railway.app/auth?error=wrong_password') {
+            url = "error"
+        }
+        const urlParams = new URLSearchParams(window.location.search);
+        const error = urlParams.get('error');
+
+        if (error === 'wrong_password') {
+            const errorMessage = document.createElement('p');
+            errorMessage.textContent = 'Wrong password. Try again.';
+            errorMessage.classList.add('error-message');
+            document.querySelector('.wrapper-form').appendChild(errorMessage);
+        }
     }
-    getURL();
+    showErrorMessage();
     function validateEmail(event) {
         const input = event.target;
         if (!input.checkValidity()) {
@@ -24,7 +36,12 @@
             input.setCustomValidity('');
         }
     }
+
+    onMount(async () => {
+      await showErrorMessage();
+    });
 </script>
+
 
 
 <svelte:head>
