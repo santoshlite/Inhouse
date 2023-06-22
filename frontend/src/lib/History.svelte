0bde949b34b7175281453e93cd2f28f21904c305
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2023-06-21 20:42:01 -0400

    Create History.svelte

---
+++ frontend/src/lib/History.svelte
@@ -0,0 +1,25 @@
+<script>
+// @ts-nocheck
+
+export let historyList = [];
+export let fetchResponse;
+
+function hasItems(list) {
+    return list && list.length > 0;
+}
+</script>
+
+<div class="wrapper">
+    <div class="title">History</div>
+
+    {#if hasItems(historyList)}
+    <div class="list-container">
+        {#each historyList as item}
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <div class="history-item" on:click={fetchResponse(item)}>📄 &nbsp; {item}</div>
+        {/each}
+    </div>
+    {:else}
+    <div class="empty-message">It's quiet here...</div>
+    {/if}
+</div>
