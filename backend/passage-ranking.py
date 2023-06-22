4c245b40d7791fa292db80c634a720cf6f7ba38a
Santosh Passoubady <61618641+ssantoshp@users.noreply.github.com>
2023-06-21 20:36:19 -0400

    Update passage-ranking.py

--- backend/passage-ranking.py
+++ backend/passage-ranking.py
@@ -22,8 +22,9 @@ db = client["userDocuments"]
 
 _image = (
     Image.debian_slim()
-    .pip_install("sentence-transformers","numpy", "pymongo", "certifi")
+    .pip_install("sentence-transformers", "numpy", "pymongo", "certifi")
 )
+
 
 @stub.function(image=_image)
 def magic(args: Dict):
@@ -43,16 +44,20 @@ def magic(args: Dict):
     for document in collection_doc.find({}):
         # If embeddings are not already stored in database, compute them
         if "Embeddings" not in document or len(document["Embeddings"]) != len(document["Blocks"]):
-            embeddings = [emb.tolist() for emb in model.encode(document["Blocks"])]
-            print("COUNT "+ str(len(embeddings)))
-            collection_doc.update_one({"_id": document["_id"]}, {"$set": {"Embeddings": embeddings}})
+            embeddings = [emb.tolist()
+                          for emb in model.encode(document["Blocks"])]
+            print("COUNT " + str(len(embeddings)))
+            collection_doc.update_one({"_id": document["_id"]}, {
+                                      "$set": {"Embeddings": embeddings}})
 
             # Convert embeddings to NumPy arrays
-            embeddings = [np.array(block_emb, dtype=np.float32) for block_emb in embeddings]
-        
+            embeddings = [np.array(block_emb, dtype=np.float32)
+                          for block_emb in embeddings]
+
         # Convert existing embeddings to NumPy arrays
         else:
-            embeddings = [np.array(block_emb, dtype=np.float32) for block_emb in document["Embeddings"]]
+            embeddings = [np.array(block_emb, dtype=np.float32)
+                          for block_emb in document["Embeddings"]]
 
         blocks.extend(document['Blocks'])
 
@@ -64,11 +69,12 @@ def magic(args: Dict):
     # Compute scores for all blocks using vectorized operation
     scores = util.dot_score(query_emb, blocks_embs).squeeze().tolist()
 
-    #Combine docs & scores
+    # Combine docs & scores
     blocks_score_pairs = list(zip(blocks, scores))
 
-    #Sort by decreasing score
-    blocks_score_pairs = sorted(blocks_score_pairs, key=lambda x: x[1], reverse=True)
+    # Sort by decreasing score
+    blocks_score_pairs = sorted(
+        blocks_score_pairs, key=lambda x: x[1], reverse=True)
 
     # Return top 10 blocks if there are more than 10 blocks
     if count_blocks < 10:
@@ -76,4 +82,4 @@ def magic(args: Dict):
     else:
         top_blocks = [item[0] for item in blocks_score_pairs[:10]]
 
-    return {"blocks" : top_blocks}
+    return {"blocks": top_blocks}
